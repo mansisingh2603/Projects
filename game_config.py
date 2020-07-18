1d2dca8efa0caa8f81d0bf1b8afeacfb2bd25903
Mansi Singh <67183714+mansisingh2603@users.noreply.github.com>
2020-07-18 13:47:00 +0530

    Update game_config.py

--- game_config.py
+++ game_config.py
@@ -1,10 +1,12 @@
 import os
 
-IMAGE_SIZE = 128  # setting image size
-SCREEN_SIZE = 512  # setting screen size
-NUM_TILES_SIDE = 4
+IMAGE_SIZE = 128        # parameters for the screen
+SCREEN_SIZE = 512       
+NUM_TILES_SIDE = 4  
 NUM_TILES_TOTAL = 16
 MARGIN = 4
-ASSET_DIR = 'assets'  # making a path to the assets folder
-ASSET_FILES = [x for x in os.listdir(ASSET_DIR) if x[-3:].lower() == 'png']  # asset files will include all png files
-assert len(ASSET_FILES) == 8  # ensuring that there are 8 images in the asset_files.
+ASSET_DIR = 'assets'     # making a path to the assets folder
+
+ASSET_FILES = [x for x in os.listdir(ASSET_DIR) if x[-3:].lower() == 'png']     # asset files will include all png files
+
+assert len(ASSET_FILES) == 8  # checking that there are 8 images in the asset_files.
