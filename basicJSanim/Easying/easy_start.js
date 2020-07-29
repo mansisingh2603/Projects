76313b4d06c546afc070646346a01054614da20c
Mansi Singh <67183714+mansisingh2603@users.noreply.github.com>
2020-07-29 20:16:05 +0530

    Add files via upload
    This small project here shows animation using the concept of easying. You can find the curve attributes at https://easings.net .

---
+++ basicJSanim/Easying/easy_start.js
@@ -0,0 +1,18 @@
+let redLight = document.querySelector("#redLight");
+let car      = document.querySelector("#car");
+
+setTimeout(()=>{
+    startAnimation();
+}, 2000)
+
+let startAnimation = () => {
+    redLight.style.display = "none";
+    let startKeyframes = {"left": "2vw"};
+    let endKeyframes = {"left": "100vw"};
+    let options = {
+        duration: 3000
+        easing:cubic-bezier(0.76, 0, 0.24, 1);; //starts slow and gains speed
+    };
+
+    car.animate([startKeyframes, endKeyframes], options);
+};
